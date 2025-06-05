b5ee8fbb9f0811119e389f19912ae6ca8a634340
hariprasad <hariprasad034@gmail.com>
2025-06-05 18:14:45 +0530

    Add content-type detection and download option

--- DocumentViewerApp/Models/DocumentViewModel.cs
+++ DocumentViewerApp/Models/DocumentViewModel.cs
@@ -3,5 +3,6 @@ namespace DocumentViewerApp.Models
     public class DocumentViewModel
     {
         public string DocumentUrl { get; set; } = string.Empty;
+        public string? ContentType { get; set; }
     }
 }
